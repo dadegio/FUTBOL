b3e24f646d2301a70c893c5e94fde5db51bf52ba
dadegio <dgiordano.davide@gmail.com>
2026-07-08 18:09:04 +0200

    rebranding according to Cammino Imperiale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
     manifest: "/manifest.json",
     icons: {
     icon: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
+    apple: "/cammino-imperiale-logo.png",
     },
 };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
